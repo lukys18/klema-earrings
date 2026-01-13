a961af5435103b8c0f16c3b6ce6abc36e5b65b13
lukys18 <bako16@uniba.sk>
2026-01-13 20:27:03 +0100

    test

--- chatbot-widget.js
+++ chatbot-widget.js
@@ -6,7 +6,7 @@
   const VERCEL_URL = "https://klema-earrings.vercel.app";
 
   // Povolene domény
-  const allowed = ["klemaearrings.sk", "https://test1-239283829347123859138772.myshopify.com", "ragnetiq.com"];
+  const allowed = ["klemaearrings.sk", "*.myshopify.com", "ragnetiq.com"];
   if (!allowed.includes(window.location.hostname)) {
     console.warn("Tento widget nie je povolený na tejto doméne");
     return; // NEvytvára iframe
